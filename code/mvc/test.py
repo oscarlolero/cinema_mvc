2eb66363214c35049e5d218270b3a2d2a090ac23
Oscar Montes <oscmcojc1@live.com.mx>
2020-05-04 14:57:01 -0500

    Especificacion

--- code/mvc/test.py
+++ code/mvc/test.py
@@ -1,4 +1,5 @@
 "****Se necesita tener instalado prettytable**** (pip install PrettyTable)"
+"Si hay boletos reservados para cierta pelicula o horario, no se podran eliminar estos ultimos."
 from controller.controller import Controller
 
 c = Controller()
